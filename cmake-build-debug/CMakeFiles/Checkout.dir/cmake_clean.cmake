89dc30a33e6af6479d7e6a694eb7797aa08a2b08
Coby Jackson <ppc2618@ic.ac.uk>
2021-04-10 16:08:33 +0100

    Pushing files

---
+++ cmake-build-debug/CMakeFiles/Checkout.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Checkout.dir/main.cpp.obj"
+  "CMakeFiles/Checkout.dir/Item.cpp.obj"
+  "CMakeFiles/Checkout.dir/Set.cpp.obj"
+  "Checkout.pdb"
+  "Checkout.exe"
+  "Checkout.exe.manifest"
+  "libCheckout.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Checkout.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
